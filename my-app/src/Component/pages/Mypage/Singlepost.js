1364aef24217f208b89723aeb0ccfd6765ec69e9
choijw1116 <choijw1116@gmail.com>
2021-02-10 03:32:44 +0900

    [fix] singlepost.js

--- my-app/src/Component/pages/Mypage/Singlepost.js
+++ my-app/src/Component/pages/Mypage/Singlepost.js
@@ -1,7 +1,16 @@
 import React from "react";
+import "./Singlepost.css";
+import img from "../../../img/dog.png";
 
 function Singlepost() {
-  return <h1>hello</h1>;
+  return (
+    <>
+      <div>
+        <img src={img}></img>
+        <p>hello It's me Olivia</p>
+      </div>
+    </>
+  );
 }
 
 export default Singlepost;
