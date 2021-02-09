2ada7047329c7beef2f27de1157be8a6a5e54a18
choijw1116 <choijw1116@gmail.com>
2021-02-09 14:10:56 +0900

    app.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -9,29 +9,26 @@ import Mypage from "./Component/pages/Mypage/Mypage";
 import Id from "./Component/pages/Mypage/id";
 import Post from "./Component/pages/Mypage/post";
 
+import Auth from "./Component/Auth/Auth";
 
-import Auth from './Component/Auth/Auth'
+function App() {
+  // const [tocken, setTocken] = useState();
 
-
-  function App() {
-    // const [tocken, setTocken] = useState();
-    
-    // if (!tocken) {
-    //   return <Auth setTocken={setTocken} />
-    // }
+  // if (!tocken) {
+  //   return <Auth setTocken={setTocken} />
+  // }
 
   return (
     <Router>
       <Header />
 
       <Switch>
-        <Route path='/' exact component={Home} /> 
-        <Route path='/theteam' component={TheTeam} />
-        <Route path='/signin' component={Auth} />
-        {/* <Route path="/mypage" component={Mypage} /> */}
+        <Route path="/" exact component={Home} />
+        <Route path="/theteam" component={TheTeam} />
+        <Route path="/signin" component={Auth} />
+        <Route path="/mypage" component={Mypage} />
       </Switch>
     </Router>
-
   );
 }
 
