c2d79196902d66f4b63b1773859b311ee1178f10
emily <emiliyoun@gmail.com>
2021-02-12 22:29:09 +0900

    [feat] update validation

---
+++ my-app/src/Component/Auth/FormSuccess.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function FormSuccess() {
+    return (
+        <div className="form-right">
+            <div className="form-success">
+                We have receivved your requested 
+            </div>
+
+        </div>
+    )
+}
+
+export default FormSuccess
